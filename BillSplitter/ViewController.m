e6f8c4c02b3216dad47260a1c963bcfe1da39d56
Frank Chen <frankcrestview@hotmail.com>
2019-05-12 16:27:20 -0400

    split bills

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
 @end
 
 @implementation ViewController
@@ -19,5 +23,19 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)buttonPressed:(UIButton *)sender {
+    double billAmount = [self.textField.text doubleValue];
+    NSUInteger numOfPeopleToSplit = self.slider.value;
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc]init];
+    [formatter setNumberStyle:NSNumberFormatterCurrencyPluralStyle];
+    double billForEachPerson = billAmount / numOfPeopleToSplit;
+    NSString* billString = [formatter stringFromNumber:[NSNumber numberWithFloat:billForEachPerson]];
+    self.label.text = billString;
+}
+
+- (IBAction)sliderValueChanged:(UISlider *)sender {
+    int discreteValue = roundl([sender value]);
+    [sender setValue:(float)discreteValue];
+}
 
 @end
